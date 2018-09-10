d086dbe148bb044c8541ca16362be80a59a17d51
Gaël Piconcely <gael.piconcely@free.fr>
2018-09-10 23:28:11 +0200

    Test PA/PR

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,10 +3,10 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const _26e7430f = () => import('..\\pages\\tens.vue' /* webpackChunkName: "pages_tens" */).then(m => m.default || m)
-const _96a0f86c = () => import('..\\pages\\pa.vue' /* webpackChunkName: "pages_pa" */).then(m => m.default || m)
-const _94c1d94a = () => import('..\\pages\\pr.vue' /* webpackChunkName: "pages_pr" */).then(m => m.default || m)
-const _4dea49ee = () => import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */).then(m => m.default || m)
+const _86908a20 = () => import('..\\pages\\tens.vue' /* webpackChunkName: "pages_tens" */).then(m => m.default || m)
+const _364e7feb = () => import('..\\pages\\pa.vue' /* webpackChunkName: "pages_pa" */).then(m => m.default || m)
+const _373e0f7c = () => import('..\\pages\\pr.vue' /* webpackChunkName: "pages_pr" */).then(m => m.default || m)
+const _056d4170 = () => import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */).then(m => m.default || m)
 
 
 
@@ -68,22 +68,22 @@ export function createRouter () {
     routes: [
 		{
 			path: "/tens",
-			component: _26e7430f,
+			component: _86908a20,
 			name: "tens"
 		},
 		{
 			path: "/pa",
-			component: _96a0f86c,
+			component: _364e7feb,
 			name: "pa"
 		},
 		{
 			path: "/pr",
-			component: _94c1d94a,
+			component: _373e0f7c,
 			name: "pr"
 		},
 		{
 			path: "/",
-			component: _4dea49ee,
+			component: _056d4170,
 			name: "index"
 		}
     ],
